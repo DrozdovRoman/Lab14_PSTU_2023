4421e20a0df744672335a8f4405692e36ed60893
Roman Drozdov <romandrozdov@icloud.com>
2023-03-28 15:23:01 +0500

    Initial commit

---
+++ Lab14_PSTU_2023/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab14_PSTU_2023
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
